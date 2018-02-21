8e2829c1c8ecdc6b0b59caaeedb0d5fad659cb01
Lukas Aldersley <aldersleluk@hotmail.com>
2018-02-21 16:50:57 +0100

    Corredted Errors, Cleaned up, added Serial Port

--- basic_test.ino
+++ basic_test.ino
@@ -1,46 +1,59 @@
 /* Air Quality Sensor (MQ135) [S037] : http://rdiot.tistory.com/110 [RDIoT Demo] */
 
+//edit by Lukas Aldersley
+
+//libraries
 #include <MQ135.h>
-#include <Wire.h> 
+#include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 
-LiquidCrystal_I2C lcd(0x27,20,4);  // LCD2004
-int pin = A0;
-int sensorValue;
-int ledPin = 13;
+//constants
+const int pin = A4;
+const long BAUD = 115200;
 
+//objects
+LiquidCrystal_I2C lcd(0x26, 20, 4); // LCD2004
 MQ135 gasSensor = MQ135(pin);
-float rzero = gasSensor.getRZero();
-int ppm = gasSensor.getPPM();
+
+
+void setup() {
+  Serial.begin(BAUD);       //Initialize Serial monitor
+  lcd.init();               //Initialize LCD
+  lcd.backlight();          //enable LCD Backlight
+}
 
 void loop()
 {
- 
-  lcd.setCursor(0,0);
-  lcd.print("S037:MQ135");
-
-
-  sensorValue = analogRead(pin);
-  lcd.setCursor(0,1);
-  lcd.print("A0=" + (String)sensorValue + " Resis="+(String)gasSensor.getResistance() +" ");
-
+  //get data
+  int sensorValue = analogRead(pin);
   float rzero = gasSensor.getRZero();
-  digitalWrite(ledPin, HIGH);      // turn the ledPin on
-  delay(100);                      // stop the program for some time
-  digitalWrite(ledPin, LOW);       // turn the ledPin off
-  delay(100);  // stop the program for some time
- 
-  lcd.setCursor(0,2);
-  lcd.print("rzero=" + (String)rzero + "    ");
- 
-
   float co2_ppm = gasSensor.getPPM();
-  int ppm = co2_ppm / 4;
   //Vrl = val * ( 5.00 / 1024.0  );      // V
-  //Rs = 20000 * ( 5.00 - Vrl) / Vrl ;   // Ohm 
-  //ratio =  Rs/Ro;     
-  lcd.setCursor(0,3);
+  //Rs = 20000 * ( 5.00 - Vrl) / Vrl ;   // Ohm
+  //ratio =  Rs/Ro;
+
+
+  //I don't know why this is relevant
+  for (int i = 0; i < 2; i++) {                               //repeat twice
+    digitalWrite(LED_BUILTIN, digitalRead(LED_BUILTIN));      //Toggle built-in LED (pin13)
+    delay(100);                                               //Wait for 1/10 Second
+  }
+
+  //Print to LCD screen
+  lcd.clear();
+  lcd.print("MQ135");
+  lcd.setCursor(0, 1);
+  lcd.print("A0=" + (String)sensorValue + " Resis=" + (String)gasSensor.getResistance() + " ");
+  lcd.setCursor(0, 2);
+  lcd.print("rzero=" + (String)rzero + "    ");
+  lcd.setCursor(0, 3);
   lcd.print("co2 ppm=" + (String)co2_ppm + "    ");
-    
-  delay(1000);
+
+  //Print to Serial monitor
+  Serial.print("A0=" + (String)sensorValue + "\tResis=" + (String)gasSensor.getResistance() + "\t");
+  Serial.print("rzero=" + (String)rzero + "\t");
+  Serial.println("co2 ppm=" + (String)co2_ppm);
+
+
+  delay(1000);    //delay for one second
 }
